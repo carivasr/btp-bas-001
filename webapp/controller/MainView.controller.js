5e574fa3d19b865950b077c2f0b7268e84208e68
carivasr <christian_rivas_90@hotmail.com>
2023-04-02 02:53:16 +0000

    Commit 01/04/2023 09:53pm

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -7,24 +7,51 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
-        return Controller.extend("logaligroup.employees.controller.MainView", {
-            onInit: function () {
+        function onInit() {
+            var oJSONModel = new sap.ui.model.json.JSONModel();
+            var oView = this.getView();
+            var i18nBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
 
-            },
+            var oJSON = {
+                employeeId : "12345",
+                countryKey : "UK",
+                listCountry: [
+                    {
+                        key: "US",
+                        text: i18nBundle.getText("countryUS")
+                    },
+                    {
+                        key: "UK",
+                        text: i18nBundle.getText("countryUK")
+                    },
+                    {
+                        key: "ES",
+                        text: i18nBundle.getText("countryES")
+                    }
+                ]
+            };
 
-            onValidate: function () {
-                var inputEmployee = this.byId("inputEmployee");
-                var valueEmployee = inputEmployee.getValue();
+            oJSONModel.setData(oJSON);
+            oView.setModel(oJSONModel);
 
-                if (valueEmployee.length === 6) {
-                    //inputEmployee.setDescription("Ok");
-                    this.byId("labelCountry").setVisible(true);
-                    this.byId("slCountry").setVisible(true);
-                } else {
-                    //inputEmployee.setDescription("Not Ok");
-                    this.byId("labelCountry").setVisible(false);
-                    this.byId("slCountry").setVisible(false);
-                };
+        };
+
+        var Main = Controller.extend("logaligroup.employees.controller.MainView", {});
+
+        Main.prototype.onValidate = function () {
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmployee = inputEmployee.getValue();
+
+            if (valueEmployee.length === 6) {
+                this.getView().byId("labelCountry").setVisible(true);
+                this.getView().byId("slCountry").setVisible(true);
+            } else {
+                this.getView().byId("labelCountry").setVisible(false);
+                this.getView().byId("slCountry").setVisible(false);
             }
-        });
+        };
+        
+        Main.prototype.onInit = onInit;
+        return Main;
+    
     });
